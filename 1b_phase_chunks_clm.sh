f58619549f2aacc2bddf1cb983b07eddfffeed88
Christina Magyar <magyar@mhgcp-h00.grid.bcm.edu>
2026-06-22 08:47:22 -0500

    1b_phase_chunks: time=8h, time-min=2h

--- 1b_phase_chunks_clm.sh
+++ 1b_phase_chunks_clm.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=1b_chunk
 #SBATCH --partition=mhgcp
 #SBATCH --exclude=mhgcp-t01,mhgcp-t02,mhgcp-t03,mhgcp-t04,mhgcp-t05,mhgcp-t06,mhgcp-t07,mhgcp-t08,mhgcp-t09,mhgcp-t10,mhgcp-t11,mhgcp-t12
-#SBATCH --time=06:00:00
+#SBATCH --time=08:00:00
 #SBATCH --time-min=02:00:00
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=12
